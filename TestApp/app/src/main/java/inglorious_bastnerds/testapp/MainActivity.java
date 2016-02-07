0c5525d7ff42b731fe471d31a799a857a55e7bc8
khirani6 <khirani6@gatech.edu>
2016-02-07 11:16:53 -0500

    Changed main title of application

--- TestApp/app/src/main/java/inglorious_bastnerds/testapp/MainActivity.java
+++ TestApp/app/src/main/java/inglorious_bastnerds/testapp/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Movie App");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
