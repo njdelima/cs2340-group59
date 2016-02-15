2ebad446a05001e336ce20287e11626bfbd9f8f0
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-02-15 02:37:22 -0500

    User class to store registered user credentials - volatile

---
+++ TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/User.java
@@ -0,0 +1,77 @@
+package ga.neerajdelima.themovieapp;
+
+import android.os.Parcelable;
+import android.os.Parcel;
+
+/**
+ * Created by Neeraj on 2/15/16.
+ */
+public class User implements Parcelable {
+    private String username;
+    private String password;
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>()
+    {
+        @Override
+        public User createFromParcel(Parcel source)
+        {
+            return new User(source);
+        }
+
+        @Override
+        public User[] newArray(int size)
+        {
+            return new User[size];
+        }
+    };
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (!(obj instanceof User)) return false;
+        if (obj == this) return true;
+
+        User that = (User) obj;
+        if ( (this.getUsername().equals(that.getUsername()) ) &&
+                ( this.getPassword().equals(that.getPassword()) ) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public User(Parcel in) {
+        this.username = in.readString();
+        this.password = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.username);
+        dest.writeString(this.password);
+    }
+}
