bb3090a2408931f6b2544e4c4b36acdc15954f06
khirani6 <khirani6@gatech.edu>
2016-02-15 13:41:48 -0500

    Basic JavaDoc for Profile Activity

--- TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/ProfileActivity.java
+++ TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/ProfileActivity.java
@@ -3,6 +3,12 @@ package ga.neerajdelima.themovieapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Profile Activity
+ * @author Komal Hirani
+ * @version 1.0
+ */
+
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
