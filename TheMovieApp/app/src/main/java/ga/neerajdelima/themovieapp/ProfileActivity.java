7525bccce629e23111b2e6014c8ce0e6e9de39dc
khirani6 <khirani6@gatech.edu>
2016-02-15 18:46:49 -0500

    Added drawer functionality to profile main page

--- TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/ProfileActivity.java
+++ TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/ProfileActivity.java
@@ -1,7 +1,15 @@
 package ga.neerajdelima.themovieapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Profile Activity
@@ -10,10 +18,46 @@ import android.os.Bundle;
  */
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    ArrayList<User> valid_credentials;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        valid_credentials = this.getIntent().getParcelableArrayListExtra("CREDENTIALS");
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        String[] optsArray = getResources().getStringArray(R.array.navigation_array);
+        addDrawerItems(optsArray);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                handleNavClick(view);
+                // Toast.makeText(HomeActivity.this, ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+    private void handleNavClick(View view) {
+        String label = ((TextView) view).getText().toString();
+        if (label.equals("Logout")) {
+            logout();
+        }
+        if (label.equals("Profile")) {
+            Intent intent = new Intent(this, ProfileActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void addDrawerItems(String[] optsArray) {
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, optsArray);
+        mDrawerList.setAdapter(mAdapter);
+    }
+
+    private void logout() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putParcelableArrayListExtra("CREDENTIALS", valid_credentials);
+        startActivity(intent);
+        finish();
+    }
+
 }
