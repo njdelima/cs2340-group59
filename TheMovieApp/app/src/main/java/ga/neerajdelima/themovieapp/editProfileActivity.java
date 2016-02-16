efd3b9c08f55308001091ea75b544e6ecf0d498f
khirani6 <khirani6@gatech.edu>
2016-02-15 22:45:27 -0500

    Able to edit username and first name

--- TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/editProfileActivity.java
+++ TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/editProfileActivity.java
@@ -6,23 +6,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import ga.neerajdelima.themovieapp.model.User;
 import ga.neerajdelima.themovieapp.model.UserModel;
 
 public class editProfileActivity extends AppCompatActivity {
     Intent intent;
     UserModel userModel;
+    User currentUser;
+    EditText userNameText;
+    EditText firstNameText;
+    String firstName;
+    String userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        intent = this.getIntent();
-        userModel = new UserModel();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        intent = this.getIntent();
+        userModel = new UserModel();
+        currentUser = userModel.getLoggedInUser();
+        //userNameText.setHint(currentUser.getUsername());
+        userNameText = (EditText) findViewById(R.id.edit_userName);
+        firstNameText = (EditText) findViewById(R.id.edit_firstName);
+        userNameText.setText(currentUser.getUsername());
+        firstNameText.setText(currentUser.getFirstName());
+
+        firstName = firstNameText.getText().toString();
+        userName = userNameText.getText().toString();
+
+        if (firstName == null) {
+            firstNameText.setText("Enter First Name Here");
+        }
     }
     public void saveChanges(View view) {
-        EditText firstNameText = (EditText) findViewById(R.id.edit_firstName);
-        String firstName = firstNameText.getText().toString();
-        if (firstName != null) {
-            userModel.getLoggedInUser().setFirstName(firstName);
+
+        userNameText.setText(userNameText.getText());
+        firstNameText.setText(firstNameText.getText());
+
+        firstName = firstNameText.getText().toString();
+        userName = userNameText.getText().toString();
+
+        if (firstName != null & userName != null) {
+            currentUser.setFirstName(firstName);
+            currentUser.setUsername(userName);
             Intent intent = new Intent(this, ProfileActivity.class);
             startActivity(intent);
         }
