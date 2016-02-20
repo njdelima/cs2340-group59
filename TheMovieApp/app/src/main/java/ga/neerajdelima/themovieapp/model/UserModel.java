45d5371b47e2d006ea665fcf1eeb364aa2d7dec2
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-02-20 15:58:03 -0500

    HTTP requests, async tasks tryout

--- TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/model/UserModel.java
+++ TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/model/UserModel.java
@@ -1,7 +1,15 @@
 package ga.neerajdelima.themovieapp.model;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import android.util.Log;
+import android.os.AsyncTask;
+import org.json.JSONObject;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by Neeraj on 2/15/16.
@@ -9,14 +17,48 @@ import android.util.Log;
 public class UserModel {
     static ArrayList<User> users = new ArrayList<User>();
 
+    private class UserTask extends AsyncTask {
+        @Override
+        protected String doInBackground(Object... args) {
+            String requestURL = (String) args[0];
+            String jsonData = (String) args[1];
+            try {
+                URL url = new URL(requestURL);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);
+                connection.setInstanceFollowRedirects(false);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("charset", "utf-8");
+                connection.setRequestProperty("Content-Length", Integer.toString(jsonData.length()));
+                connection.setUseCaches(false);
+
+                byte[] postData = jsonData.getBytes(StandardCharsets.UTF_8);
+                connection.getOutputStream().write(postData);
+                return connection.getResponseMessage();
+            } catch (MalformedURLException e) {
+                Log.d("Malformed URL", e.getMessage());
+            } catch (IOException e) {
+                Log.d("IOException", e.getMessage());
+            }
+            return null;
+        }
+
+        protected void onPostExecute(String response) {
+            Log.d("Response", response);
+        }
+    }
+
     public boolean checkLogin(String username, String password) {
         User toCheck = new User(username, password);
         return users.contains(toCheck);
     }
 
     public void addUser(String username, String password) {
-        User toAdd = new User(username, password);
-        users.add(toAdd);
+        String data = "{\"username\":\"tanay\",\"password\",\"12345\"}";
+        new UserTask().execute("http://128.61.104.207:2340/api/users/add.php", data);
+        //User toAdd = new User(username, password);
+        //users.add(toAdd);
     }
 
     public void logAllUsers() {
