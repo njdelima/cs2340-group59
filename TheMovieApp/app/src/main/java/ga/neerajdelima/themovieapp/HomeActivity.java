b5fcdd5ca369c8283b831a33099063e3af6f8a78
emestanza3 <eduardomestnza_96@gatech.edu>
2016-04-03 21:38:59 -0400

    nevermind checkstyle was right but still fuck checkstyle

--- TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/HomeActivity.java
+++ TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/HomeActivity.java
@@ -26,7 +26,6 @@ import ga.neerajdelima.themovieapp.model.network.FetchTopMoviesResponse;
  */
 
 public class HomeActivity extends AppCompatActivity implements FetchTopMoviesResponse {
-    private List<Movie> results;
     private ListView mDrawerList;
     private ArrayAdapter<String> mAdapter;
     private ArrayAdapter<String> ad;
--- TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/editProfileActivity.java
+++ TheMovieApp/app/src/main/java/ga/neerajdelima/themovieapp/editProfileActivity.java
@@ -19,7 +19,6 @@ import ga.neerajdelima.themovieapp.model.UserModel;
  */
 
 public class editProfileActivity extends AppCompatActivity {
-    private Intent intent;
     private UserModel userModel;
     private User currentUser;
     private EditText userNameText;
