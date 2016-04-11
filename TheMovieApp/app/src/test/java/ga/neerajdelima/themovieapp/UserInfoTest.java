dba5ae6fb12fa494a30ed52ea60f33fb45884419
mlee432 <leepogii@gmail.com>
2016-04-11 11:51:01 -0400

    Edit

--- TheMovieApp/app/src/test/java/ga/neerajdelima/themovieapp/UserInfoTest.java
+++ TheMovieApp/app/src/test/java/ga/neerajdelima/themovieapp/UserInfoTest.java
@@ -2,6 +2,9 @@ package ga.neerajdelima.themovieapp;
 
 import android.test.InstrumentationTestCase;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import ga.neerajdelima.themovieapp.model.User;
@@ -15,38 +18,32 @@ public class UserInfoTest extends InstrumentationTestCase {
     UserModel userModel;
     List<User> users;
 
-    public void testUserInfo() throws Exception {
-        userModel = new UserModel();
-        userModel.setLoggedInUser("minho");
-        Thread.sleep(10000);
-        users.add(userModel.getLoggedInUser());
-        userModel = new UserModel();
-        userModel.setLoggedInUser("emma");
-        users.add(userModel.getLoggedInUser());
-        userModel = new UserModel();
-        userModel.setLoggedInUser("eduardo");
-        users.add(userModel.getLoggedInUser());
-        userModel = new UserModel();
-        userModel.setLoggedInUser("komal");
-        users.add(userModel.getLoggedInUser());
-        userModel = new UserModel();
-        userModel.setLoggedInUser("neeraj");
-        users.add(userModel.getLoggedInUser());
+    public void setUp() {
+        users = new ArrayList<>();
+        users.add(new User("minho","pass","min","lee","Computer Science", false, false, true));
+        users.add(new User("komal","word","kimal","lee","Industrial Engineering", false, true, true));
+        users.add(new User("neeraj","pass","neeraj","super","Applied Mathematics", true, false, false));
+        users.add(new User("edu","word","edu","champ","Computer Science", false, true, false));
+        users.add(new User("emma","pass","emma","watson","Aerospace Engineering", true, false, false));
+        testUserInfo();
+    }
+    @Test
+    public void testUserInfo() {
+        assertEquals(false, users.get(0).isLocked());
+        assertEquals(false, users.get(0).isBanned());
         assertEquals(true, users.get(0).isAdmin());
-        assertEquals(true, users.get(0).isBanned());
-        assertEquals(true, users.get(0).isLocked());
-        assertEquals(true, users.get(1).isAdmin());
+        assertEquals(false, users.get(1).isLocked());
         assertEquals(true, users.get(1).isBanned());
-        assertEquals(true, users.get(1).isLocked());
-        assertEquals(true, users.get(2).isAdmin());
-        assertEquals(true ,users.get(2).isBanned());
-        assertEquals(true ,users.get(2).isLocked());
-        assertEquals(true, users.get(3).isAdmin());
-        assertEquals(true ,users.get(3).isBanned());
-        assertEquals(true ,users.get(3).isLocked());
-        assertEquals(true, users.get(4).isAdmin());
-        assertEquals(true ,users.get(4).isBanned());
-        assertEquals(true ,users.get(4).isLocked());
+        assertEquals(true, users.get(1).isAdmin());
+        assertEquals(true, users.get(2).isLocked());
+        assertEquals(false, users.get(2).isBanned());
+        assertEquals(false, users.get(2).isAdmin());
+        assertEquals(false, users.get(3).isLocked());
+        assertEquals(true, users.get(3).isBanned());
+        assertEquals(false, users.get(3).isAdmin());
+        assertEquals(true, users.get(4).isLocked());
+        assertEquals(false, users.get(4).isBanned());
+        assertEquals(false, users.get(4).isAdmin());
     }
 
 
