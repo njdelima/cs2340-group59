ceac668ddf738e7e74409c2652c54438ee237461
mlee432 <leepogii@gmail.com>
2016-04-11 11:58:10 -0400

    Edit

--- TheMovieApp/app/src/test/java/ga/neerajdelima/themovieapp/UserInfoTest.java
+++ TheMovieApp/app/src/test/java/ga/neerajdelima/themovieapp/UserInfoTest.java
@@ -32,18 +32,30 @@ public class UserInfoTest extends InstrumentationTestCase {
         assertEquals(false, users.get(0).isLocked());
         assertEquals(false, users.get(0).isBanned());
         assertEquals(true, users.get(0).isAdmin());
+        users.get(0).setLocked(true);
+        assertEquals(true, users.get(0).isLocked()); // locked
         assertEquals(false, users.get(1).isLocked());
         assertEquals(true, users.get(1).isBanned());
+        users.get(1).setBanned(false);
+        assertEquals(false, users.get(1).isBanned());
         assertEquals(true, users.get(1).isAdmin());
         assertEquals(true, users.get(2).isLocked());
+        users.get(2).setLocked(false);
+        assertEquals(false, users.get(2).isLocked());
         assertEquals(false, users.get(2).isBanned());
         assertEquals(false, users.get(2).isAdmin());
+        users.get(2).setAdmin(true);
+        assertEquals(true, users.get(2).isAdmin());
         assertEquals(false, users.get(3).isLocked());
         assertEquals(true, users.get(3).isBanned());
         assertEquals(false, users.get(3).isAdmin());
         assertEquals(true, users.get(4).isLocked());
+        users.get(4).setLocked(false);
+        assertEquals(false, users.get(4).isLocked());
         assertEquals(false, users.get(4).isBanned());
         assertEquals(false, users.get(4).isAdmin());
+        users.get(4).setAdmin(true);
+        assertEquals(true, users.get(4).isAdmin());
     }
 
 
