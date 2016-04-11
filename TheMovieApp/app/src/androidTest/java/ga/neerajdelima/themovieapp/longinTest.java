c3085075abff17a92abf172c4603df6966501407
mlee432 <leepogii@gmail.com>
2016-04-11 10:35:19 -0400

    1

--- TheMovieApp/app/src/androidTest/java/ga/neerajdelima/themovieapp/longinTest.java
+++ TheMovieApp/app/src/androidTest/java/ga/neerajdelima/themovieapp/longinTest.java
@@ -1,6 +1,15 @@
 package ga.neerajdelima.themovieapp;
+
+import ga.neerajdelima.themovieapp.model.UserModel;
+
 /**
  * Created by minholee on 4/5/16.
  */
 public class longinTest {
+    UserModel user = new UserModel();
+    public void test() {
+        user.setLoggedInUser("jae");
+        user.getLoggedInUser();
+
+    }
 }
